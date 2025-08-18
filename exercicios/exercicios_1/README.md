b2fd8f621a76c4d7946ab7d78ff8455032f491ec
osmarwilliam <osmarwilliam4@gmail.com>
2025-08-18 20:47:18 -0300

    Adicionando exercicio de um conversor de moedas

---
+++ exercicios/exercicios_1/README.md
@@ -0,0 +1,3 @@
+# CONVERSOR DE MOEDAS
+Um simples conversor de algumas moedas específicas, que consome uma api e usa a biblioteca `readline-sync` para interagir diretamente pelo terminal
+
---
+++ exercicios/exercicios_1/index.js
@@ -0,0 +1,71 @@
+// algoritmo simples que consume uma api e converte moedas
+
+const apiKey = '77478b4a4fb9e95cd5cdc6d7';
+const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+async function getExchangeRate(daMoeda, paraMoeda) {
+    
+    try {
+        const response = await fetch(`${apiURL}${daMoeda}`)
+        // pego o resultado de response e transformo em json para manipular
+        const data = await response.json();
+
+        if (data.result === "success"){
+            return data.conversion_rates[paraMoeda];
+
+        } else{
+            throw new Error("Erro ao buscar a taxa de câmbio");
+        }
+
+    }catch(error) {
+        console.log("error: ", error);
+        return null;
+    }
+
+}
+
+// getExchangeRate("USD", "BRL")
+
+const readlineSync = require('readline-sync');
+
+async function main() {
+    let valor; // undefined, portanto é falsy iniciamente
+    while (!valor) {
+        valor = Number(readlineSync.question("Digite um valor para conversão: "));
+        if (isNaN(valor)) {
+            console.log("Digite um número válido.");
+            valor = undefined; // reseta para continuar no loop
+        }
+    }
+
+    const moedas = ['USD', 'EUR', 'CAD', 'GBP', 'BRL'];
+    let daMoeda;
+    let paraMoeda;
+
+    try {
+        daMoeda = readlineSync.keyInSelect(moedas, 'Escolha a moeda que iremos converter: ');
+        if (daMoeda === -1) {
+            console.log("Você escolheu cancelar a operação.");
+            return;
+        }
+        paraMoeda = readlineSync.keyInSelect(moedas, 'Escolha a moeda para qual iremos converter: ');
+        if (paraMoeda === -1) {
+            console.log("Você escolheu cancelar a operação.");
+            return;
+        }
+    } catch (error) {
+        console.log("error", error);
+        return;
+    }
+
+    const exchangeRate = await getExchangeRate(moedas[daMoeda], moedas[paraMoeda]);
+
+    if (exchangeRate) {
+        const valorConvertido = valor * exchangeRate;
+        console.log(`A conversão de ${valor} ${moedas[daMoeda]} para ${moedas[paraMoeda]} é: ${valorConvertido.toFixed(2)}`);
+    } else {
+        console.log("Não foi possível realizar a conversão.");
+    }
+}
+
+main();
