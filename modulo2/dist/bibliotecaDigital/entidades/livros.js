94e27375d73f51eea35b69ee6af0944f21707cca
osmarwilliam <osmarwilliam4@gmail.com>
2025-09-20 20:53:34 -0300

    removendo arquivos dist

--- modulo2/dist/bibliotecaDigital/entidades/livros.js
+++
@@ -1,9 +0,0 @@
-export class Livros {
-    idBook;
-    nomeBook;
-    constructor(idBook, nomeBook) {
-        this.idBook = idBook;
-        this.nomeBook = nomeBook;
-    }
-}
-//# sourceMappingURL=livros.js.map
--- modulo2/dist/bibliotecaDigital/main.js
+++
@@ -1,8 +0,0 @@
-import { Livros } from "./entidades/livros.js";
-import { Autor } from "./entidades/autor.js";
-const autor1 = new Autor("George Orwell", 46, 1);
-const livro1 = new Livros("LIV001", "1984");
-autor1.adicionarLivros(livro1);
-console.log(autor1);
-console.log("ola");
-//# sourceMappingURL=main.js.map
